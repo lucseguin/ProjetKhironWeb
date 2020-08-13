74e70f3e69832790636bb1cab975010cbddc63d4
Luc Seguin <lseguin2010@gmail.com>
2020-08-13 09:04:00 -0400

    move to https

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-axios.defaults.baseURL = "http://projetkhiron.com:3000";
+axios.defaults.baseURL = "https://projetkhiron.com:3000";
 
 if(process.env.REACT_APP_PK_DB_API_ENDPOINT && process.env.REACT_APP_PK_DB_API_ENDPOINT.trim().length > 0) {
   console.log("process.env.REACT_APP_PK_DB_API_ENDPOINT:"+process.env.REACT_APP_PK_DB_API_ENDPOINT);
